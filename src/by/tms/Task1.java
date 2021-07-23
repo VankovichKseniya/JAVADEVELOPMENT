5606b7a3f758b65dcc5615459d146b1f32e5588b
Xuysha Vankovich <vankovich861@gmail.com>
2021-07-23 15:39:32 +0300

    save tasks

---
+++ src/by/tms/Task1.java
@@ -0,0 +1,11 @@
+package by.tms;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+	int i = 4;
+	int c = 3;
+
+        System.out.println(Math.pow(i, c) + " - результат возведения числа 4 в куб");
+    }
+}
---
+++ src/by/tms/Task2.java
@@ -0,0 +1,7 @@
+package by.tms;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+    }
+}
