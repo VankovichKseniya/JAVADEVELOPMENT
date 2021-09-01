74a1afde753b895c505390649d4fa19a1016db38
Xuysha Vankovich <vankovich861@gmail.com>
2021-09-01 13:55:48 +0300

    add lesson 7 - main

--- src/by/tms/lesson7/Main.java
+++ src/by/tms/lesson7/Main.java
@@ -1,4 +1,13 @@
-package by.tms;
+package by.tms.lesson7;
+
+import java.io.IOException;
 
 public class Main {
+
+    public static void main(String[] args) throws IOException {
+//        Task1.getSumChars(new File("./resourses/textPoem"));
+
+        Task2.fileWithRandomNumbers();
+        Task2.InputRandomNumbers("FileForWriting.txt", "FileWithSortNumbers");
+    }
 }
